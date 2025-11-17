f1709518625c6eed5bd00012e46281d9ded2b2fb
ivannargl <r.guzmanlopez89@gmail.com>
2025-11-17 12:15:53 -0600

    MS Storage para Roster

---
+++ storage-ms/src/main/java/mx/edu/uteq/idgs12/storage_ms/StorageMsApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.storage_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageMsApplication.class, args);
+	}
+
+}
